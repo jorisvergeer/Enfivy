6a99e2011685f0504418ffaf263edb6987216231
Joris Vergeer <j.vergeer@agis.nl>
2017-01-30 11:23:25 +0100

    Add logging

--- Fody/ModuleWeaver.cs
+++ Fody/ModuleWeaver.cs
@@ -31,15 +31,19 @@ public class ModuleWeaver
         {
             if (config.AttributeNames.Contains(customAttribute.AttributeType.Name))
             {
+                LogInfo.Invoke("Found attribute: " + customAttribute.AttributeType.Name);
                 for (int i = 0; i < customAttribute.ConstructorArguments.Count; i++)
                 {
+                    LogInfo.Invoke($"Attribute has {customAttribute.ConstructorArguments.Count} constructor arguments");
                     var arg = customAttribute.ConstructorArguments[i];
                     if (arg.Value is string)
                     {
+                        LogInfo.Invoke($"Constructor argument index {i} is a string, replacing tokens");
                         var result = arg.Value as string;
 
                         foreach (Match match in Regex.Matches((string) arg.Value, $"{config.Preamble}({config.Regex}){config.Postamble}"))
                         {
+                            LogInfo.Invoke($"Found {match.Value}");
                             var replaceWith = string.Empty;
                             var signature = match.Value;
 
@@ -47,21 +51,30 @@ public class ModuleWeaver
                             {
                                 var key = match.Groups[1].Value;
 
+                                LogInfo.Invoke($"Using key: {key}");
+                                LogInfo.Invoke("Looking for environment var");
                                 replaceWith = Environment.GetEnvironmentVariable(key);
                                 if (replaceWith == null)
                                 {
+                                    LogInfo.Invoke("Looking for default ");
                                     replaceWith = config.Defaults.Where(pair => pair.Key == key).Select(pair => pair.Value).FirstOrDefault();
                                 }
                                 if (replaceWith == null)
                                 {
+                                    LogInfo.Invoke("No vlaue found");
                                     replaceWith = string.Empty;
                                 }
                             }
 
+                            LogInfo.Invoke($"Replacing {signature} with {replaceWith}");
                             result = result.Replace(signature, replaceWith);
                         }
 
                         customAttribute.ConstructorArguments[i] = new CustomAttributeArgument(ModuleDefinition.TypeSystem.String, result);
+                    }
+                    else
+                    {
+                        LogInfo.Invoke($"Constructor argument index {i} is no string");
                     }
                 }
             }
